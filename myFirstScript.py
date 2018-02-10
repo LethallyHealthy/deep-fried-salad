c3f032954f840011a9c1d03ca30fe35f62d42ba5
LethallyHealthy <36343709+LethallyHealthy@users.noreply.github.com>
2018-02-10 16:35:08 -0500

    2/10/2018 - Intro to RWC coding.
    One script does square roots. Other is first script that we messed around with.

---
+++ myFirstScript.py
@@ -0,0 +1,22 @@
+#This is my first script
+
+
+'''
+
+'''
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+num1= int(num1)
+num2= int(num2)
+
+num3 = num1+num2
+
+print("The result of your first and second numbers: " + str(num3))
+end = input("")
+again = input("again")
+
+
+
+
